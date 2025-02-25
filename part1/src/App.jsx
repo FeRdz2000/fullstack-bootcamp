696e0f2b414966f854a06acbe767db6223a5e276
unknown <rfemario@gmail.com>
2025-02-25 15:56:25 -0400

    first commit

---
+++ part1/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Message from './Message.jsx';
+
+function App() {
+  return (
+    <div className='App'>
+      <Message color='red' message='Iniciando en' />
+      <Message color='green' message='React' />
+    </div>
+  )
+}
+
+export default App
